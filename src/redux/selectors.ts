b1a71e82ad740910a8b861068da51b6e6ef09a29
artem <spiridovichartem99@gmail.com>
2020-09-11 13:57:05 +0300

    fix search bar

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -1,5 +1,10 @@
 import {InitialStateType} from "./post-reducer";
 
 export const GetItems = (state: InitialStateType) => {
-  return state.posts.filter(e => state.filter.toLowerCase() === e.title.slice(0, state.filter.length).toLowerCase())
+  // return state.posts.filter(e => state.filter.toLowerCase() === e.title.slice(0, state.filter.length).toLowerCase())
+  return state.posts.filter(e => {
+    if(e.title.toLowerCase().indexOf(state.filter.toLowerCase()) !== -1) {
+      return e
+    }
+  })
 }

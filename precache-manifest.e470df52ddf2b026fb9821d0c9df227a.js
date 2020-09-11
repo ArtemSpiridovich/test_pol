6e5231e3fcb9710096fa1aaad425c9d67458fcb0
artem <spiridovichartem99@gmail.com>
2020-09-11 12:36:11 +0300

    Updates

---
+++ precache-manifest.e470df52ddf2b026fb9821d0c9df227a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0097858ef317ea4d601642cba4f2038",
+    "url": "/test_pol/index.html"
+  },
+  {
+    "revision": "8a3f269b524dae4841e6",
+    "url": "/test_pol/static/css/2.34c0de18.chunk.css"
+  },
+  {
+    "revision": "1f6d1b65298a8ed239e8",
+    "url": "/test_pol/static/css/main.189f46aa.chunk.css"
+  },
+  {
+    "revision": "8a3f269b524dae4841e6",
+    "url": "/test_pol/static/js/2.fef865c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test_pol/static/js/2.fef865c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f6d1b65298a8ed239e8",
+    "url": "/test_pol/static/js/main.ade75018.chunk.js"
+  },
+  {
+    "revision": "33253b0e1365b300e272",
+    "url": "/test_pol/static/js/runtime-main.fafe2996.js"
+  },
+  {
+    "revision": "d57a85e78e70e31ab41adc491a9f6e30",
+    "url": "/test_pol/static/media/carousel.d57a85e7.svg"
+  },
+  {
+    "revision": "27ce550f17d3bea73922f7077a1eeb38",
+    "url": "/test_pol/static/media/grid.27ce550f.svg"
+  },
+  {
+    "revision": "dde0180d1a6a0a0cd8d9d13862d84d90",
+    "url": "/test_pol/static/media/left-arrow-dis.dde0180d.svg"
+  },
+  {
+    "revision": "87e27296ef01f81f0346150ed8e58f11",
+    "url": "/test_pol/static/media/left-arrow.87e27296.svg"
+  },
+  {
+    "revision": "859a8b022f860680310aff900ad2206e",
+    "url": "/test_pol/static/media/logo.859a8b02.svg"
+  },
+  {
+    "revision": "ae13456f69270b72e3c4727979005dad",
+    "url": "/test_pol/static/media/right-arrow-dis.ae13456f.svg"
+  },
+  {
+    "revision": "a6dfbed73934e95ab6f08feabed4da00",
+    "url": "/test_pol/static/media/right-arrow.a6dfbed7.svg"
+  }
+]);
